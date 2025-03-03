de033a52075ace5b421ef834b72c9a8e5c99394e
SH <sohaibhamdi55@outlook.com>
2025-03-03 14:08:55 -0400

    Seance 1,2,3,4

--- Numpy.py
+++ Numpy.py
@@ -76,7 +76,7 @@ print(np9[0:2 , 1:3]) # 0:2 is the first row to 2 - 1 = 1 row and 1:3 is columns
 print(np9[0,[1,3]])  # array[row_index, [column_indices]]
 
 
-# ----------------------------------Seance 2---------------------------------------------
+# ----------------------------------Seance 3---------------------------------------------
 
 # Numpy Universal Function 
 np10 = np.array([0,1,-2,3,4,5,6,7,8])
